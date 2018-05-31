1e18b82d9fd5cc5a8d8a24e2ae7dec5ac5b9a0c4
zzKool <zzKool@protonmail.com>
2018-05-31 04:58:27 +0200

    key bug fix

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // Figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
 	//prod
-	modules.exports = require('./prod');
+	module.exports = require('./prod');
 } else {
 	//dev
 	module.exports = require('./dev');
