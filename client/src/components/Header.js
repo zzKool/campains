d146a263a2334a278feb3e0956535a614f131fc0
zzKool <zzKool@protonmail.com>
2018-06-08 01:13:23 +0200

    7.82 - Header Content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,11 +5,19 @@ class Header extends Component {
 	renderContent() {
 		switch (this.props.auth) {
 			case null:
-				return 'Still deciding';
+				return;
 			case false:
-				return 'im logged out';
+				return (
+					<li>
+						<a href="/auth/google">Login with Google</a>
+					</li>
+				);
 			default:
-				return 'im logged in';
+				return (
+					<li>
+						<a>Logout</a>
+					</li>
+				);
 		}
 	}
 
